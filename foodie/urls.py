21cadf19086e57bec4283615d5e0c2ae14a5439c
naveen <g.naveenkumar0607@gmail.com>
2021-03-07 20:05:52 +0530

    detailed pending

--- foodie/urls.py
+++ foodie/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', f_views.gpage, name='greeting'),
     path('item/new/', ItemCreateView.as_view(), name='new'),
     path('item/<int:pk>/update/', ItemUpdateView.as_view(), name='update'),
-    path('item/<int:pk>/delete/', ItemDeleteView.as_view(template_name='foodie/confirm_delete.html'), name='delete')
+    path('item/<int:pk>/delete/', ItemDeleteView.as_view(template_name='foodie/confirm_delete.html'), name='delete'),
+    path()
 ]
